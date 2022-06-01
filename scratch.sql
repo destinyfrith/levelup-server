6cfd86da664c1c77b4eaacdb1b4b258e0d7b6b66
Destiny Frith <desilyvahn@gmail.com>
2022-06-01 11:25:40 -0500

    reports and html templates added for games and users

--- scratch.sql
+++ scratch.sql
@@ -7,3 +7,13 @@ SELECT * FROM levelupapi_gamer;
 
 SELECT * FROM levelupapi_game;
 SELECT * FROM levelupapi_event;
+
+SELECT 
+            g.title,
+            u.first_name || " " || u.last_name AS full_name,
+            gmr.id
+            FROM levelupapi_game as g
+            JOIN levelupapi_gamer as gmr
+            ON g.gamer_id = gmr.id
+            JOIN auth_user u
+            ON u.id = gmr.id
